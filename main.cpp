75aa2b7ddc4179d7eb06fcb5b4c979fedb58225f
Semyon Trafnyak <m2202962@edu.misis.ru>
2023-09-30 01:54:57 +0300

    fix pdf2img

--- main.cpp
+++ main.cpp
@@ -8,40 +8,61 @@
 #include <poppler-image.h>
 
 //#include "lib\some_heck\some_check.h"
+class pdf2img{
+	public:
+    explicit pdf2img(const std::string& filename,int DPI){
+    	poppler::document* mypdf = poppler::document::load_from_file(filename);
 
-cv::Mat readPDFtoCV(const std::string& filename,int DPI) {
-    poppler::document* mypdf = poppler::document::load_from_file(filename);
+	    if(mypdf == NULL) {
+	        std::cerr << "couldn't read pdf\n";
+	    }
 
-    if(mypdf == NULL) {
-        std::cerr << "couldn't read pdf\n";
-        return cv::Mat();
+	    std::cout << "pdf has " << mypdf->pages() << " pages\n";
+	    capacity_ = mypdf->pages();
+	    data_ = new cv::Mat [capacity_];
+
+		for(int i = 0; i < mypdf->pages(); i++){
+			poppler::page* mypage = mypdf->create_page(i);
+		    poppler::page_renderer renderer;
+		    renderer.set_render_hint(poppler::page_renderer::text_antialiasing);
+		    poppler::image myimage = renderer.render_page(mypage,DPI,DPI);
+
+		    std::cout << "created image of  " << myimage.width() << "x"<< myimage.height() << "\n";
+		    cv::Mat cvimg;
+		    if(myimage.format() == poppler::image::format_rgb24) {
+		        cv::Mat(myimage.height(),myimage.width(),CV_8UC3,myimage.data()).copyTo(cvimg);
+		    } else if(myimage.format() == poppler::image::format_argb32) {
+		        cv::Mat(myimage.height(),myimage.width(),CV_8UC4,myimage.data()).copyTo(cvimg);
+		    } else {
+		        std::cerr << "PDF format no good\n";
+		    }
+		    *(data_ + i) = cvimg;
+		}
     }
 
-    std::cout << "pdf has " << mypdf->pages() << " pages\n";
 
-    poppler::page* mypage = mypdf->create_page(0);
-    poppler::page_renderer renderer;
-    renderer.set_render_hint(poppler::page_renderer::text_antialiasing);
-    poppler::image myimage = renderer.render_page(mypage,DPI,DPI);
-
-    std::cout << "created image of  " << myimage.width() << "x"<< myimage.height() << "\n";
-    cv::Mat cvimg;
-    if(myimage.format() == poppler::image::format_rgb24) {
-        cv::Mat(myimage.height(),myimage.width(),CV_8UC3,myimage.data()).copyTo(cvimg);
-    } else if(myimage.format() == poppler::image::format_argb32) {
-        cv::Mat(myimage.height(),myimage.width(),CV_8UC4,myimage.data()).copyTo(cvimg);
-    } else {
-        std::cerr << "PDF format no good\n";
-        return cv::Mat();
-    }
-    return cvimg;
-}
+    cv::Mat& operator[] (const std::ptrdiff_t i)
+	{
+	    if (i < 0 || i >= capacity_)
+	        throw std::out_of_range("Wrong Index");
+	    return *(data_ + i);
+	}
+	ptrdiff_t get_size(){
+		return capacity_;
+	}
+private:
+    ptrdiff_t capacity_ = 0;
+	cv::Mat* data_ = nullptr;
+};
 
 int main(int argc, char* argv[]){
 	for(int i = 0; i < argc; i++){
 		std::cout << argv[i] << std::endl;
 	}
-	
-	imshow("Display window", readPDFtoCV(argv[1], 300));
-    int k = cv::waitKey(0);
+	pdf2img images_from_pdf(argv[1], 300);
+
+	for(int i = 0; i < images_from_pdf.get_size(); i++){
+		imshow("Display window", images_from_pdf[i]);
+	    cv::waitKey(0);
+	}
 }
