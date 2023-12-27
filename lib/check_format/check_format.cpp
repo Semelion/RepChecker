bee5b898aaff8d5dba766d6b5a445f6667d42e26
Чекунова Марьяна <113853811+marjana2213@users.noreply.github.com>
2023-12-27 04:08:00 +0300

    fix mm error

--- lib/check_format/check_format.cpp
+++ lib/check_format/check_format.cpp
@@ -5,25 +5,25 @@ std::vector<std::string> isFormat(pdf2img imgs, double fault, double dpi) {
 	double px_in_mm = dpi / 25.4;
 
 	for (int i = 0; i < imgs.get_size(); i++) {
-		if (abs(imgs[i].size().width - (105 * px_in_mm)) <= fault && abs(imgs[i].size().height - (148 * px_in_mm)) <= fault * px_in_mm) {
+		if (abs(imgs[i].size().width - (105 * px_in_mm)) <= fault * px_in_mm && abs(imgs[i].size().height - (148 * px_in_mm)) <= fault * px_in_mm) {
 			formats.push_back("A6");
 		}
-		else if (abs(imgs[i].size().width - (148 * px_in_mm)) <= fault && abs(imgs[i].size().height - (210 * px_in_mm)) <= fault * px_in_mm) {
+		else if (abs(imgs[i].size().width - (148 * px_in_mm)) <= fault * px_in_mm && abs(imgs[i].size().height - (210 * px_in_mm)) <= fault * px_in_mm) {
 			formats.push_back("A5");
 		}
-		else if (abs(imgs[i].size().width - (210 * px_in_mm)) <= fault && abs(imgs[i].size().height - (297 * px_in_mm)) <= fault * px_in_mm) {
+		else if (abs(imgs[i].size().width - (210 * px_in_mm)) <= fault * px_in_mm && abs(imgs[i].size().height - (297 * px_in_mm)) <= fault * px_in_mm) {
 			formats.push_back("A4");
 		}
-		else if (abs(imgs[i].size().width - (297 * px_in_mm)) <= fault && abs(imgs[i].size().height - (420 * px_in_mm)) <= fault * px_in_mm) {
+		else if (abs(imgs[i].size().width - (297 * px_in_mm)) <= fault * px_in_mm && abs(imgs[i].size().height - (420 * px_in_mm)) <= fault * px_in_mm) {
 			formats.push_back("A3");
 		}
-		else if (abs(imgs[i].size().width - (420 * px_in_mm)) <= fault && abs(imgs[i].size().height - (594 * px_in_mm)) <= fault * px_in_mm) {
+		else if (abs(imgs[i].size().width - (420 * px_in_mm)) <= fault * px_in_mm && abs(imgs[i].size().height - (594 * px_in_mm)) <= fault * px_in_mm) {
 			formats.push_back("A2");
 		}
-		else if (abs(imgs[i].size().width - (594 * px_in_mm)) <= fault && abs(imgs[i].size().height - (841 * px_in_mm)) <= fault * px_in_mm) {
+		else if (abs(imgs[i].size().width - (594 * px_in_mm)) <= fault * px_in_mm && abs(imgs[i].size().height - (841 * px_in_mm)) <= fault * px_in_mm) {
 			formats.push_back("A1");
 		}
-		else if (abs(imgs[i].size().width - (841 * px_in_mm)) <= fault && abs(imgs[i].size().height - (1189 * px_in_mm)) <= fault * px_in_mm) {
+		else if (abs(imgs[i].size().width - (841 * px_in_mm)) <= fault * px_in_mm && abs(imgs[i].size().height - (1189 * px_in_mm)) <= fault * px_in_mm) {
 			formats.push_back("A0");
 		}
 		else {
