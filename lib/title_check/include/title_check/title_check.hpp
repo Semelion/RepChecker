de909096ed52eddc93c4c97d1a723b4edb8a13fa
daryavanina <114079693+daryavanina@users.noreply.github.com>
2024-01-06 17:09:08 +0300

    Update title_check.hpp

--- lib/title_check/include/title_check/title_check.hpp
+++ lib/title_check/include/title_check/title_check.hpp
@@ -21,33 +21,33 @@ public:
 	std::string get_result();
 private:
 	std::string centering_blocks = "";
+
+public:
+	/**
+		* @brief Проверка блока на выравнивание по середине
+		* @param x_left абсцисса левого края блока
+		* @param x_right абсцисса правого края блока
+		* @param sheet_width высота блока
+		* @param admission погрешность
+		* @param dpi разрешение
+		* @return true или false
+		*/
+	bool check_centering_text(const int& x_left, const int& x_right, const int& sheet_width,
+		const int& admission, const int& dpi = 300);
+
+	/**
+		* @brief Проверка блока на выравнивание по правому краю
+		* @return численный ответ
+		*/
+	bool check_centering_right_text(const int& x_left, const int& x_right, const int& sheet_width,
+		const int& admission, const int& dpi = 300);
+
+	/**
+		* @brief Проверка блока на выравнивание по левому краю
+		* @return численный ответ
+		*/
+	bool check_centering_left_text(const int& x_left, const int& x_right, const int& sheet_width,
+		const int& admission, const int& dpi = 300);
+
 };
-
-/**
-	* @brief Проверка блока на выравнивание по середине
-	* @param x_left абсцисса левого края блока
-	* @param x_right абсцисса правого края блока
-	* @param sheet_width высота блока
-	* @param admission погрешность
-	* @param dpi разрешение
-	* @return true или false
-	*/
-bool check_centering_text(const int& x_left, const int& x_right, const int& sheet_width,
-	const int& admission,const int& dpi = 300);
-
-/**
-	* @brief Проверка блока на выравнивание по правому краю
-	* @return численный ответ
-	*/
-bool check_centering_right_text(const int& x_left, const int& x_right, const int& sheet_width,
-	const int& admission, const int& dpi = 300);
-
-/**
-	* @brief Проверка блока на выравнивание по левому краю
-	* @return численный ответ
-	*/
-bool check_centering_left_text(const int& x_left, const int& x_right, const int& sheet_width,
-	const int& admission, const int& dpi = 300);
-
-
 #endif
